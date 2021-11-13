a7bf734220de5f9221a51b2ad87ea8b1857e3aba
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-11-14 00:02:31 +0900

    First tab done

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          final friendPosts = snapshot.data?.friendPosts ?? [];
+
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: recipes),
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: friendPosts),
+            ],
+          );
+        }
+
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
