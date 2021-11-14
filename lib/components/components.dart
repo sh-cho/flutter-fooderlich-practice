658bfc1a0e4cd6ccb1ab1614194325643695741c
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-11-14 14:50:20 +0900

    Second tab done

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -5,4 +5,6 @@ export 'card3.dart';
 export 'circle_image.dart';
 export 'friend_post_list_view.dart';
 export 'friend_post_tile.dart';
+export 'recipe_thumbnail.dart';
+export 'recipes_grid_view.dart';
 export 'today_recipe_list_view.dart';
